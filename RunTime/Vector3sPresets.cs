771b8c77696f266639743de10632fd4b3e471aae
Darsan Pakeerathan <Darsan1991@gmail.com>
2024-06-04 17:50:27 +0530

    added vector3 arrays

---
+++ RunTime/Vector3sPresets.cs
@@ -0,0 +1,21 @@
+﻿using DGames.Essentials.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace DGames.Presets
+{
+    [DashboardResourceItem(tabPath:"Presets/Vector/Vector3",subFolderPath:DEFAULT_FOLDER_PATH,displayName:"Root - Key Array")]
+    
+    [CreateAssetMenu]
+    // ReSharper disable once InconsistentNaming
+    public class Vector3sPresets : DirectPresets<Vector3[]>
+    {
+#if UNITY_EDITOR
+        [MenuItem("MyGames/Presets/Vector3s")]
+        public static void Open()
+        {
+            Editor.ScriptableEditorUtils.OpenOrCreateDefault<Vector3sPresets>(childrenPath: DEFAULT_FOLDER_PATH);
+        }
+#endif
+    }
+}
